2894c605e0ba82b5e3db2de81da2b04fe862859f
gogosauce <hgo.roger@wanadoo.fr>
2017-01-10 17:59:33 +0100

    Update Case.java

--- Case.java
+++ Case.java
@@ -1,48 +1,58 @@
 
 
 public class Case {
-    private State etat;
-    private int val; // le chiffre contenu dans la case
-    private Type type;
-    
-    public enum State {
-	Discovered,
-	Hidden,
-	Flag;
-    }
+	private State etat;
+	private int val, x, y; // le chiffre contenu dans la case
+	private Type type;
 
-    public enum Type {
-	Empty,
-	Mine;
-    }
-    
-    public Case() {
-	etat = State.Hidden;
-	val  = 0;
-	type = Type.Empty;
-    }
-	
-    public int getVal() {
-	return val;
-    }
-	
-    public void setVal(int x) {
-	val = x;
-    }
-	
-    public State getEtat() {
-	return etat;
-    }
-	
-    public void setEtat(State etat) {
-	this.etat = etat;
-    }
+	public enum State {
+		Discovered,
+		Hidden,
+		Flag;
+	}
 
-    public Type getType() {
-	return type;
-    }
-	
-    public void setEtat(Type type) {
-	this.type = type;
-    }
+	public enum Type {
+		Empty,
+		Mine;
+	}
+
+	public Case(int x, int y) {
+		etat = State.Hidden;
+		val  = 0;
+		type = Type.Empty;
+		this.x = x;
+		this.y = y;
+	}
+
+	public int getVal() {
+		return val;
+	}
+
+	public void setVal(int x) {
+		val = x;
+	}
+
+	public State getState() {
+		return etat;
+	}
+
+	public void setState(State etat) {
+		this.etat = etat;
+	}
+
+	public Type getType() {
+		return type;
+	}
+
+	public void setType(Type type) {
+		this.type = type;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
 }
