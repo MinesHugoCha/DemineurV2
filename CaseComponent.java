9bbda6c8e822d48d5262c598e8ac14ffb0b5adaa
gogosauce <hgo.roger@wanadoo.fr>
2017-01-11 20:48:17 +0100

    Update CaseComponent.java

--- CaseComponent.java
+++ CaseComponent.java
@@ -1,5 +1,7 @@
+import java.awt.AlphaComposite;
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
@@ -8,9 +10,8 @@ import javax.swing.JComponent;
 
 public class CaseComponent extends JComponent {
 	private Case refCase;
-	private int width = 20;
-
-	public CaseComponent (Case refCase)
+	private int width = 20;		
+	public CaseComponent (Case refCase, Grille grille, CaseComponent[][] grilleAffichage)
 	{
 		super();
 		this.setSize(width, width);
@@ -19,33 +20,35 @@ public class CaseComponent extends JComponent {
 		this.addMouseListener(new MouseListener() {
 			@Override
 			public void mousePressed(MouseEvent e) {
-				// TODO Auto-generated method stub
-				System.out.println("X: " + refCase.getX() + "Y: " + refCase.getY());
+				// TODO Auto-generated method stub				
 			}
 
 			@Override
 			public void mouseClicked(MouseEvent e) {
 				// TODO Auto-generated method stub
-
 			}
 
 			@Override
 			public void mouseEntered(MouseEvent e) {
 				// TODO Auto-generated method stub
-
 			}
 
 			@Override
 			public void mouseExited(MouseEvent e) {
 				// TODO Auto-generated method stub
-
 			}
 
 			@Override
 			public void mouseReleased(MouseEvent e) {
 				refCase.setState(Case.State.Discovered);
-				repaint();
-
+				if (refCase.getType() == Case.Type.Empty && grille.getCase(refCase.getX(), refCase.getY()).getVal() == 0){ 
+						// on decouvre les cases autour
+						grille.decouvrezero_rec(refCase.getX(), refCase.getY());						
+					}				
+				else if(refCase.getType() == Case.Type.Mine){
+					System.out.println("vous avez perdu!");	
+				}
+				repaint();				
 			}
 		});
 	}
@@ -63,14 +66,13 @@ public class CaseComponent extends JComponent {
 			g.drawImage(new ImageIcon("-1.png").getImage(), 0, 0, null);					
 		}
 		if(refCase.getState() == Case.State.Hidden){
-			//Graphics2D g2d =(Graphics2D) g;
-			//g2d.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, .8F));
+						Graphics2D g2d =(Graphics2D) g;
+						g2d.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, .9F));
 			g.drawImage(new ImageIcon("caseN.png").getImage(), 0, 0, null);
-		}		
+		}
 	}
 
 	public int getWidth() {
 		return this.width;
 	}
-
 }
