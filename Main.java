346aa0cb2e4ed1319b9f5a1a8f83baf29ff372c8
Szarlotka <leon.charlott@gmail.com>
2017-01-15 21:59:25 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,8 +1,13 @@
 public class Main {
 	public static void main (String[] args){
 		Grille grille = new Grille(1);
+//		Grille grilleint = new Grille(2);
+//		Grille grillediff = new Grille(3);
+		
 		grille.placeMines();
 		grille.incremCadre();
+		
 		Frame fram = new Frame(grille);
+	
 	}
 }
