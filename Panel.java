af4a07049b519fcf2f1bef7af8cad3b28f87507d
Szarlotka <leon.charlott@gmail.com>
2017-01-15 22:01:17 +0100

    Update Panel.java

--- Panel.java
+++ Panel.java
@@ -1,5 +1,10 @@
+
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 import javax.swing.JPanel;
 
@@ -10,8 +15,43 @@ public class Panel extends JPanel{
 		super();
 		this.grille = grille;
 		this.setLayout(null);
+		//repaint();
+	
+		this.addMouseListener(new MouseListener() {
+			
+			@Override
+			public void mouseReleased(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void mousePressed(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void mouseExited(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				// TODO Auto-generated method stub
+				repaint();
+				System.out.println("pouet");
+			}
+		});
 	}
-
+	
 	public void paintComponent(Graphics g){		
 		g.setColor(Color.white);		
 		g.fillRect(0,  0,  getSize().width, getSize().height);
@@ -24,4 +64,15 @@ public class Panel extends JPanel{
 			}
 		}
 	}
+
+	
 }
+
+/*
+class Zoom6000 implements ActionListener{
+public void actionPerformed(ActionEvent e){
+    outputGUIView.graphPanel.setPreferredSize(new  Dimension(screenWidth, 6000));
+    outputGUIView.graphScrollPanel.updateUI();
+
+}
+}*/
