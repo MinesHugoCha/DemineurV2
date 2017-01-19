b7c636c865ae2e0384503087f7c83dee648da06a
Szarlotka <leon.charlott@gmail.com>
2017-01-19 17:46:58 +0100

    Update Panel.java

--- Panel.java
+++ Panel.java
@@ -16,40 +16,6 @@ public class Panel extends JPanel{
 		this.grille = grille;
 		this.setLayout(null);
 		//repaint();
-	
-		this.addMouseListener(new MouseListener() {
-			
-			@Override
-			public void mouseReleased(MouseEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void mousePressed(MouseEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void mouseExited(MouseEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void mouseEntered(MouseEvent e) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				// TODO Auto-generated method stub
-				repaint();
-				System.out.println("pouet");
-			}
-		});
 	}
 	
 	public void paintComponent(Graphics g){		
