a678f674eb899c7e5138f3fd7d85b792240cf689
gogosauce <hgo.roger@wanadoo.fr>
2017-01-19 19:16:16 +0100

    Update Case.java

--- Case.java
+++ Case.java
@@ -1,14 +1,25 @@
+/**
+ * Case de le grille dans laquelle on place les mines, valeurs et drapeaux.
+ * Une case est contenue dans une grille.
+ */
 public class Case {
 	private State etat;
-	private int val, x, y; // le chiffre contenu dans la case
 	private Type type;
+	private int val, x, y; // le chiffre contenu dans la case
+	
 
+	/**
+	 *State est une énumération de trois valeurs pour indiquer si la case est découverte, cachée ou avec un drapeau. 
+	 */
 	public enum State {
 		Discovered,
 		Hidden,
 		Flag;
 	}
 
+	/**
+	 * Type est une énumération de deux valeur pour indiquer la présence de mine ou non
+	 */
 	public enum Type {
 		Empty,
 		Mine;
